c3ce11418931bda7f4a4de8a493ca61c15a8741e
VitaliyR <vitaliy.rakov@gmail.com>
2020-12-03 09:25:40 +0300

    created empty project and added method main

---
+++ lesson_1/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    // 1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
