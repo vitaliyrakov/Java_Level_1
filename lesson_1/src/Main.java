774e8b07b6bf24b4758e14ab4bba3e39da9ab225
VitaliyR <vitaliy.rakov@gmail.com>
2020-12-03 09:28:08 +0300

    created veriables all types and inited them

--- lesson_1/src/Main.java
+++ lesson_1/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     // 1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        // 2. Создать переменные всех пройденных типов данных, и инициализировать их значения;
+        byte byteVar = 100;
+        short shortVar = 200;
+        int intVar = -1000;
+        long longVar = 2000L;
+
+        float floatVar = 10.5f;
+        double doubleVar = 20.5;
+
+        boolean boolVar = true;
+
+        char charVar = 'q';
+        String stringVar = "hello";
+
     }
 }
