29d9647eb7f7e7ded751b3ecbb10d8b838ec78eb
vitaliyR <74015913+vitaliyrakov@users.noreply.github.com>
2020-12-21 14:37:05 +0300

    Update Animal.java

--- lesson_6/src/Animal.java
+++ lesson_6/src/Animal.java
@@ -1,5 +1,8 @@
 abstract public class Animal {
+    Random rand = new Random();
+    
     protected int dispersLimit = 20; //in percent
+    protected double dispersLimitK = (1 - dispersLimit/100.0+rand.nextInt(dispersLimit*2)/100.0);
 
     protected abstract void run(int length);
 
